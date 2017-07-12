4e75a01b79f05c006bdd4651503f258a125e11ef
cglewis <clewis@iqt.org>
2017-07-12 11:25:49 -0700

    add simple gpu example

---
+++ gpu_example/example.py
@@ -0,0 +1,25 @@
+import json
+import sys
+import numpy as np
+import tensorflow as tf
+
+# Read in data from file
+data = np.genfromtxt(sys.argv[1], delimiter=',')
+data = np.reshape(data,(2,2))
+
+# Generate graph of gpu computations
+graph = tf.Graph()
+with graph.as_default():
+    with tf.device('/cpu:0'):
+        X = tf.placeholder("float64",[2,2])
+        npmatrix = np.array([[10.0, 2.0], [-5.0, 8.78]])
+        matrix = tf.Variable(npmatrix))
+        y = tf.matmul(X, matrix)
+
+    # Run the computations on the input
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        output = sess.run(y, {X: data})
+
+# print out results
+print(json.dumps(output.flatten()))
