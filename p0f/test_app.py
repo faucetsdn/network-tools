1a43f583e4d98f1c975dd7a965cc4a9ad94af629
cglewis <clewis@iqt.org>
2018-12-20 11:51:07 -0800

    fix tests

--- p0f/test_app.py
+++ p0f/test_app.py
@@ -18,21 +18,21 @@ from .app import main
 def test_run_p0f():
     with open('/tmp/test', 'w') as f:
         f.write("this is an invalid test file")
-    sys.argv = ['/tmp/test']
+    sys.argv = ['p0f', '/tmp/test']
     run_p0f()
 
 
 def test_run_tshark():
     with open('/tmp/test', 'w') as f:
         f.write("this is an invalid test file")
-    sys.argv = ['/tmp/test']
+    sys.argv = ['p0f', '/tmp/test']
     run_tshark()
 
 
 def test_parse_output():
     with open('/tmp/test', 'w') as f:
         f.write("this is an invalid test file")
-    sys.argv = ['/tmp/test']
+    sys.argv = ['p0f', '/tmp/test']
     run_p0f()
     run_tshark()
     parse_output()
@@ -44,14 +44,15 @@ def test_connect():
 
 
 def test_save():
+    r = connect()
     results = {'foo':{'bar':'baz'}}
-    save(results)
+    save(r, results)
     results = [{'foo':{'bar':'baz'}}]
-    save(results)
+    save(r, results)
 
 
 def test_main():
     with open('/tmp/test', 'w') as f:
         f.write("this is an invalid test file")
-    sys.argv = ['/tmp/test']
+    sys.argv = ['p0f', '/tmp/test']
     main()
