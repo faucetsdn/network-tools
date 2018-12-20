2d30cb04c66ea8ef283e98956a263615951c5216
cglewis <clewis@iqt.org>
2018-12-20 12:41:31 -0800

    more test coverage

--- tcprewrite_dot1q/test_tcprewrite.py
+++ tcprewrite_dot1q/test_tcprewrite.py
@@ -19,5 +19,8 @@ def test_get_path():
 
 def test_run_tool():
     with open('/tmp/test', 'w') as f:
+        f.write("")
+    run_tool('/tmp/test')
+    with open('/tmp/test', 'w') as f:
         f.write("This is an invalid test")
     run_tool('/tmp/test')
