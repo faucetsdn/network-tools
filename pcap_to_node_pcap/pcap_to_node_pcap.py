85efb2ec5072850f0fdf1b3fc32fb1030d35b966
cglewis <clewis@iqt.org>
2019-07-30 14:15:11 -0700

    closes #163 and closes #164

--- pcap_to_node_pcap/pcap_to_node_pcap.py
+++ pcap_to_node_pcap/pcap_to_node_pcap.py
@@ -7,10 +7,39 @@ Created on 17 July 2017
 """
 
 import datetime
+import json
 import os
 import shlex
 import subprocess
 import sys
+
+import pika
+
+
+def connect_rabbit(host='messenger', port=5672, queue='task_queue'):
+    params = pika.ConnectionParameters(host=host, port=port)
+    connection = pika.BlockingConnection(params)
+    channel = connection.channel()
+    channel.queue_declare(queue=queue, durable=True)
+    return channel
+
+def send_rabbit_msg(msg, channel, exchange='', routing_key='task_queue'):
+    channel.basic_publish(exchange=exchange,
+                          routing_key=routing_key,
+                          body=json.dumps(msg),
+                          properties=pika.BasicProperties(
+                          delivery_mode=2,
+                         ))
+    print(" [X] %s UTC %r %r" % (str(datetime.datetime.utcnow()),
+                                 str(msg['id']), str(msg['pcap_path'])))
+    return
+
+def get_version():
+    version = ''
+    with open('VERSION', 'r') as f:
+        for line in f:
+            version = line.strip()
+    return version
 
 def get_path():
     path = None
@@ -49,8 +78,19 @@ def run_tool(path):
                                           path + " -o " + output_dir + '/servers' + " -m server-ip"))
     except Exception as e:
         print(str(e))
+    return output_dir + '/clients'
 
 if __name__ == '__main__':  # pragma: no cover
     path = get_path()
     if path:
-        run_tool(path)
+        result_path = run_tool(path)
+    uid = ''
+    if 'id' in os.environ:
+        uid = os.environ['id']
+    if 'rabbit' in os.environ and os.environ['rabbit'] == 'true':
+        try:
+            channel = connect_rabbit()
+            body = {'id': uid, 'type': 'metadata', 'file_path': result_path, 'data': '', 'results': {'tool': 'pcap-splitter', 'version': get_version()}}
+            send_rabbit_msg(body, channel)
+        except Exception as e:
+            print(str(e))
