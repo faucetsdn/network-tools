19063b742c8e99e0c881bc70d5a28f8c9ff6edf5
Josh Bailey <josh@vandervecken.com>
2019-10-30 16:29:00 +1300

    Optionally annotate pcap using tshark protocol detection.

--- pcap_to_node_pcap/pcap_to_node_pcap.py
+++ pcap_to_node_pcap/pcap_to_node_pcap.py
@@ -6,7 +6,9 @@ Created on 17 July 2017
 @author: Blake Pagon
 """
 
+import argparse
 import datetime
+import ipaddress
 import json
 import os
 import shlex
@@ -15,6 +17,47 @@ import sys
 
 import pika
 
+
+def ipaddress_fields(json_fields):
+    ipas = set()
+    for _, content in sorted(json_fields.items()):
+        try:
+            ipa = ipaddress.ip_address(content)
+        except ValueError:
+            continue
+        ipas.add(str(ipa))
+    return ipas
+
+def proto_annotate_pcaps(pcap_dir):
+    pap_filenames = [
+        pcap.path for pcap in os.scandir(pcap_dir)
+        if pcap.is_file() and pcap.path.endswith('pcap')]
+    for pcap_filename in pap_filenames:
+        try:
+            pcap_json = json.loads(
+                subprocess.check_output(
+                    ['tshark', '-T', 'json', '-c', str(10), '-r', pcap_filename]))
+        except (json.decoder.JSONDecodeError, subprocess.CalledProcessError) as e:
+            print(pcapfilename, str(e))
+            continue
+        pcap_layers = []
+        for packet_json in pcap_json:
+            try:
+                layers_json = packet_json['_source']['layers']
+            except KeyError:
+                continue
+            ipas = set()
+            for field in ('ip', 'ip6', 'arp'):
+                if field in layers_json:
+                    ipas = ipas.union(ipaddress_fields(layers_json[field]))
+            packet_layers = list(ipas) + list(layers_json.keys())
+            if len(packet_layers) > len(pcap_layers):
+                pcap_layers = packet_layers
+        pcap_basename = os.path.basename(pcap_filename)
+        layers_str = '-'.join(pcap_layers)
+        layers_pcap_filename = pcap_filename.replace(
+            pcap_basename, '-'.join((layers_str, pcap_basename)))
+        os.rename(pcap_filename, layers_pcap_filename)
 
 def connect_rabbit(host='messenger', port=5672, queue='task_queue'):
     params = pika.ConnectionParameters(host=host, port=port)
@@ -41,15 +84,15 @@ def get_version():
             version = line.strip()
     return version
 
-def get_path():
+def get_path(paths):
     path = None
     try:
-        path = sys.argv[1]
+        path = paths[0]
     except Exception as e:
         print("No path provided: {0}, quitting".format(str(e)))
     return path
 
-def run_tool(path):
+def run_tool(path, protoannotate):
     if os.path.getsize(path) < 100:
        print("pcap file too small, not splitting")
        return
@@ -70,20 +113,32 @@ def run_tool(path):
         os.mkdir(output_dir + '/servers')
     except OSError:  # pragma: no cover
         print("couldn't make directories for output of this tool")
+    clients_dir = os.path.join(output_dir, 'clients')
+    servers_dir = os.path.join(output_dir, 'servers')
+
     try:
         subprocess.check_call(shlex.split("./PcapSplitter -f " +
-                                          path + " -o " + output_dir + '/clients' + " -m client-ip"))
+                                          path + " -o " + clients_dir + " -m client-ip"))
 
         subprocess.check_call(shlex.split("./PcapSplitter -f " +
-                                          path + " -o " + output_dir + '/servers' + " -m server-ip"))
+                                          path + " -o " + servers_dir + " -m server-ip"))
     except Exception as e:
         print(str(e))
+
+    if protoannotate:
+        for pcap_dir in (clients_dir, servers_dir):
+            proto_annotate_pcaps(pcap_dir)
+
     return output_dir + '/clients'
 
 if __name__ == '__main__':  # pragma: no cover
-    path = get_path()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--protoannotate', help='use tshark to annotate pcaps with protocol', action='store_true')
+    parser.add_argument('paths', nargs='*')
+    args = parser.parse_args()
+    path = get_path(args.paths)
     if path:
-        result_path = run_tool(path)
+        result_path = run_tool(path, args.protoannotate)
     uid = ''
     if 'id' in os.environ:
         uid = os.environ['id']
