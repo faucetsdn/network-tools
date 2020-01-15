198ca0bc1ccf523f19834c50700e2afe626390d0
cglewis <clewis@iqt.org>
2020-01-15 09:11:58 -0800

    release v0.11.1

--- mercury/test_app.py
+++ mercury/test_app.py
@@ -23,7 +23,7 @@ def test_ispcap():
 
 
 def test_version():
-    assert get_version() == '0.11.1.dev'
+    assert get_version() == '0.11.1'
 
 
 def test_parse_output():
--- network_tap/ncontrol/paths.py
+++ network_tap/ncontrol/paths.py
@@ -78,7 +78,7 @@ class CreateR(object):
             cmd += ' ' + payload['id'] + ' ' + payload['iters'] + ' "'
             cmd += payload['filter'] + '"'
             try:
-                container = c.containers.run(image='cyberreboot/ncapture:latest',
+                container = c.containers.run(image='cyberreboot/ncapture:v0.11.1',
                                              command=cmd, remove=True, detach=True, **tool_d)
                 resp.body = "(True, 'successfully created and started filter: " + \
                     str(payload['id']) + ' on container: ' + \
@@ -182,7 +182,7 @@ class ListR(object):
             for c in containers.containers.list(all=True):
                 # TODO: maybe find a way to not have to hard code image name
                 if c.attrs['Config']['Image'] == \
-                        'cyberreboot/ncapture:latest':
+                        'cyberreboot/ncapture:v0.11.1':
                     lst = {}
                     lst['id'] = c.attrs['Id'][:12]
                     lst['status'] = c.attrs['State']['Status']
